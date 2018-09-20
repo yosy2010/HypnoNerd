e7d62fc58e5a7ded7cbaec8ecbd6c009ec956468
Yasser Altamimi <yosy2010@gmail.com>
2018-09-20 04:33:45 +0300

    Initial Commit

---
+++ HypnoNerd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HypnoNerd
+//
+//  Created by YASSER ALTAMIMI on 20/09/2018.
+//  Copyright © 2018 YASSER ALTAMIMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
